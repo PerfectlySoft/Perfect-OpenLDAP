e5181f6193d44cb5c5f04d84e4c4fc666878abea
Rockford Wei <rocky@perfect.org>
2017-01-25 11:13:13 -0500

    Adding SASL default structure support

--- openldap.h
+++ openldap.h
@@ -1,5 +1,19 @@
+#ifndef __PERFECT_OPENLDAP__
+#define __PERFECT_OPENLDAP__
 #ifdef __APPLE__
 #include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/LDAP.framework/Headers/ldap.h"
 #else
 #include "/usr/include/ldap.h"
+#endif
+
+typedef struct lutil_sasl_defaults_s {
+	char *mech;
+	char *realm;
+	char *authcid;
+	char *passwd;
+	char *authzid;
+	char **resps;
+	int nresps;
+} lutilSASLdefaults;
+
 #endif
