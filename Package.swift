00a0b80a7a13773a278d00ba7a5ef5891494f934
Sergey Balalaev <sof.bix@mail.ru>
2019-03-27 11:11:15 +0400

    spm4 supporting

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "OpenLDAP",
     providers: [
-      .Apt("libldap2-dev")
+        .brew(["libldap2-dev"])
     ]
 )
